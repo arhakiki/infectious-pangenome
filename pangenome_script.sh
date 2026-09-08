bb8d7e60f6d159e03797874cc836e47306c1d1f0
Arini Hakiki <312784856+arhakiki@users.noreply.github.com>
2026-09-08 08:34:56 +0000

    add pangenomics analysis script.

--- pangenome_script.sh
+++ pangenome_script.sh
@@ -1,7 +1,11 @@
-# set PATH everytime we start so hopefully we don't break the server permanently
-PATH=$PATH:/home/ubuntu/Pangloss:/home/ubuntu/exonerate:/home/ubuntu/TransDecoder-TransDecoder-v5.5.0
+# set PATH everytime we start so hopefully we never break the server PATH permanently
+PATH=$PATH:/home/ubuntu/Pangloss:/home/ubuntu/exonerate:/home/ubuntu/gmes_linux_64_4:/home/ubuntu/TransDecoder-TransDecoder-v5.5.0:/home/ubuntu/ncbi-blast-2.17.0+/bin
 echo $PATH
+
+# activate conda environment for 
+conda activate pangloss
 
 # make dataset folder, then copy config file
 mkdir Fungomics
-cp  
+cd Fungomics
+cp /home/ubuntu/Pangloss/config.ini .
